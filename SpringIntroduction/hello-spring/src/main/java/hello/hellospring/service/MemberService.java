86c81fed44e1828ac05c1e7e92dca07d2bf64269
beautiflow <1114ny@naver.com>
2023-02-02 13:35:43 +0900

    feat: Section.7-1 AOP가 필요한 상황

--- SpringIntroduction/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ SpringIntroduction/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -22,10 +22,21 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원 X
-        validateDuplicateMember(member);  // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+        try{
+            // 같은 이름이 있는 중복 회원 X
+            validateDuplicateMember(member);  // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+           long finish = System.currentTimeMillis();
+           long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
+
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -39,7 +50,14 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        }finally {
+           long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
