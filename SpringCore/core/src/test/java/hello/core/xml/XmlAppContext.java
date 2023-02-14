e90b1c0ddbb9d2817dd6646bd64f3c3d692abb9f
beautiflow <1114ny@naver.com>
2023-02-14 13:14:10 +0900

    feat: Section.4-6 BeanFactory 와 ApplicationContext

---
+++ SpringCore/core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,17 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext { // xml 설정 정보 작성해보기
+
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
