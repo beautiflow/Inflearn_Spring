9f72b4ab094d8b0763e2d631a9be0aa63ad5499c
beautiflow <1114ny@naver.com>
2023-10-11 09:18:21 +0900

    feat: Section.6-2 탐색 위치와 기본 스캔 대상

--- SpringCore/core/src/main/java/hello/core/AutoAppConfig.java
+++ SpringCore/core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan ( // 스프링빈을 모두 모아서 올려야하기 때문에 필요
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
     )
 public class AutoAppConfig {
