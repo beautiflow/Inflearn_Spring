6d3c1abb8bc892498724fb383b7570a99c2077e0
beautiflow <1114ny@naver.com>
2023-10-12 09:51:20 +0900

    feat: Section.7-4 롬복과 최신 트렌드

--- SpringCore/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringCore/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,19 +3,16 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
-    private  MemberRepository memberRepository;
-    private  DiscountPolicy discountPolicy;
-
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
