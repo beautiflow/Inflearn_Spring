6abd7489b4b3398792cc49dba4797bd94c597c50
beautiflow <1114ny@naver.com>
2023-10-13 08:36:14 +0900

    feat: Section.7-5 조회 빈이 2개 이상 - 문제

--- SpringCore/core/src/main/java/hello/core/AppConfig.java
+++ SpringCore/core/src/main/java/hello/core/AppConfig.java
@@ -28,8 +28,7 @@ public class AppConfig {
     @Bean
     public OrderService orderService(){
         System.out.println("call AppConfig.orderService");
-        //return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
-        return null;
+        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
     }
 
     @Bean
