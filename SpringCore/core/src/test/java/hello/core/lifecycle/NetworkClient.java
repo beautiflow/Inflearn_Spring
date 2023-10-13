d332e1764ff890f01b1196930673e9e362d9b732
beautiflow <1114ny@naver.com>
2023-10-13 16:28:10 +0900

    feat: Section.8-2 인터페이스 InitializingBean, DisposableBean

--- SpringCore/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ SpringCore/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url){
@@ -26,5 +27,17 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect(){
         System.out.println("close " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {  // 의존관계 주입이 끝나면 호출해 주겠다는 의미
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
