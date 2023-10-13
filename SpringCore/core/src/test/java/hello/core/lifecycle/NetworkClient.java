0bf044ade290681fda540b1c2b6e41cb739680eb
beautiflow <1114ny@naver.com>
2023-10-13 16:40:15 +0900

    feat: Section.8-4 애노테이션 @PostConstruct, @PreDestory

--- SpringCore/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ SpringCore/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient  {
 
     private String url;
@@ -26,12 +29,14 @@ public class NetworkClient  {
         System.out.println("close " + url);
     }
 
+    @PostConstruct
     public void init() throws Exception {  // 의존관계 주입이 끝나면 호출해 주겠다는 의미
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disconnect();
