f520d5b90486e32cbcf2642b16a02841e450ed37
beautiflow <1114ny@naver.com>
2023-10-16 07:00:26 +0900

    feat: Section.9-6 스코프와 Provider

--- SpringCore/core/src/main/java/hello/core/web/LogDemoController.java
+++ SpringCore/core/src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +13,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request){
-       String requestURL = request.getRequestURL().toString();
-       myLogger.setRequestURL(requestURL);
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
+
+        myLogger.setRequestURL(requestURL);
 
        myLogger.log("controller test");
+       Thread.sleep(1000);
        logDemoService.logic("testId");
         return "OK";
     }
