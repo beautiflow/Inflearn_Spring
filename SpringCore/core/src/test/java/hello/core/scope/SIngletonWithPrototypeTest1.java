d316ba294273bd38fd910298f37c56f5e777c1b4
beautiflow <1114ny@naver.com>
2023-10-16 00:07:08 +0900

    feat: Section.9-4 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제해결

--- SpringCore/core/src/test/java/hello/core/scope/SIngletonWithPrototypeTest1.java
+++ SpringCore/core/src/test/java/hello/core/scope/SIngletonWithPrototypeTest1.java
@@ -4,6 +4,8 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import jakarta.inject.Provider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -35,43 +37,24 @@ public class SIngletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
-            return prototypeBean.getCount();
+            int count = prototypeBean.getCount();
+            return count;
         }
 
     }
-
-    @Scope("singleton")
-    static class ClientBean2{
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입 x02
-
-        @Autowired
-        public ClientBean2(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
-
-        public int logic(){
-            prototypeBean.addCount();
-            return prototypeBean.getCount();
-        }
-
-    }
-
-
 
     @Scope("prototype")
     static class PrototypeBean{
