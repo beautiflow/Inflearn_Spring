7d264940b792198b636e8bfab8d755ddeff373d9
beautiflow <1114ny@naver.com>
2023-10-20 12:24:18 +0900

    chore: delete comment

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -28,7 +28,6 @@ public class FrontControllerServletV1 extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("FrontControllerServletV1.service");
 
-        // /front-controller/v1/members
         String requestURI = request.getRequestURI();
 
         ControllerV1 controller = controllerMap.get(requestURI);
