c7281f90259084f008a38a48c22c11189233a9c3
beautiflow <1114ny@naver.com>
2023-10-24 21:24:08 +0900

    feat: Section.7-11 RedirectAttributes

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -56,7 +57,7 @@ public class BasicItemController {
     }
 
     // @PostMapping ("/add")
-    public String addItemV2(@ModelAttribute("item")Item item, Model model){
+    public String addItemV2(@ModelAttribute("item")Item item){
 
         itemRepository.save(item);
   //      model.addAttribute("item", item);  // 자동 추가, 생략 가능
@@ -65,22 +66,30 @@ public class BasicItemController {
     }
 
    // @PostMapping ("/add")
-    public String addItemV3(@ModelAttribute Item item, Model model){
+    public String addItemV3(@ModelAttribute Item item){
        //  Item -> item 가 modelAttribute 에 담기게 된다
         itemRepository.save(item);
         return "basic/item";
     }
 
    // @PostMapping ("/add")
-    public String addItemV4(Item item, Model model){
+    public String addItemV4(Item item){
         itemRepository.save(item);
         return "basic/item";
     }
 
-    @PostMapping ("/add")
-    public String addItemV5(Item item, Model model){
+   // @PostMapping ("/add")
+    public String addItemV5(Item item){
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping ("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
