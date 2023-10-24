5d2839e53ba7509165974ab0ffed8a9ea703e4ff
beautiflow <1114ny@naver.com>
2023-10-24 12:16:09 +0900

    feat: Section.7-8 상품 등록 처리 - @ModelAttribute

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -4,11 +4,9 @@ import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -39,9 +37,44 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+   // @PostMapping ("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model){
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    // @PostMapping ("/add")
+    public String addItemV2(@ModelAttribute("item")Item item, Model model){
+
+        itemRepository.save(item);
+  //      model.addAttribute("item", item);  // 자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
+   // @PostMapping ("/add")
+    public String addItemV3(@ModelAttribute Item item, Model model){
+       //  Item -> item 가 modelAttribute 에 담기게 된다
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
     @PostMapping ("/add")
-    public String save(){
-        return "xxx";
+    public String addItemV4(Item item, Model model){
+        itemRepository.save(item);
+        return "basic/item";
     }
 
 
