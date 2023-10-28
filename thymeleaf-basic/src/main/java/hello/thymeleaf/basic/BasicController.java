923943172e652cf316e3b304e9dc0965210ccc92
beautiflow <1114ny@naver.com>
2023-10-28 13:45:35 +0900

    feat: Section.1-15 자바스크립트 인라인

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -118,6 +118,15 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+
+        model.addAttribute("user", new User("User\"A\"", 10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model){
         List<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
