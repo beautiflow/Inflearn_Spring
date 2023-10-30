dd887fcf2e96de10d7f2602ae59844a146e52a05
beautiflow <1114ny@naver.com>
2023-10-30 21:30:07 +0900

    feat: Section.5-8 Bean Validation - 한계

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
+    @NotNull // 수정 요구사항 추가
     private Long id;
 
     @NotBlank(message = "공백X")
@@ -21,7 +22,7 @@ public class Item {
     private Integer price;  // A, typeMismatch
 
     @NotNull
-    @Max(9999)
+    //@Max(9999) // 수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
