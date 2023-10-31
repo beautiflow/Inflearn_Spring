481c6b89a3e1b9bec22391fbf338c6944e8d8d57
beautiflow <1114ny@naver.com>
2023-10-31 09:35:02 +0900

    feat: Section.5-12 Form 전송 객체 분리 - 개발

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,18 +11,18 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
+    //@NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    //@NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName; // A
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+   // @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+   // @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;  // A, typeMismatch
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class}) // 수정 요구사항 추가
+   // @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+   // @Max(value = 9999, groups = {SaveCheck.class}) // 수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
