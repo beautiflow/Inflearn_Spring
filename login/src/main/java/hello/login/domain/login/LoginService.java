75fece0e4445697cbefa5ef5013f76e7cbee6749
beautiflow <1114ny@naver.com>
2023-11-01 09:20:56 +0900

    feat: Section.6-6 로그인 처리하기 - 쿠키사용

--- login/src/main/java/hello/login/domain/login/LoginService.java
+++ login/src/main/java/hello/login/domain/login/LoginService.java
@@ -5,8 +5,6 @@ import hello.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class LoginService {
@@ -19,14 +17,6 @@ public class LoginService {
      */
 
     public Member login(String loginId, String password){
-//        Optional<Member> findMemberOptional = memberRepository.findByLoginId(loginId);
-//        Member member = findMemberOptional.get();
-//        if(member.getPassword().equals(password)){
-//            return member;
-//        }
-//        else {
-//            return null;
-//        }
         return memberRepository.findByLoginId(loginId)
                 .filter( m -> m.getPassword().equals(password))
                 .orElse(null);
