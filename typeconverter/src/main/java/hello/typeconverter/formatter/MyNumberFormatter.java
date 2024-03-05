2c6194d6faae3eb164cb227609d1e31a4f5c1bd2
beautiflow <1114ny@naver.com>
2024-03-05 14:25:36 +0900

    feat: Section.10-7 포맷터 - Formatter

---
+++ typeconverter/src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
@@ -0,0 +1,26 @@
+package hello.typeconverter.formatter;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.Formatter;
+
+@Slf4j
+public class MyNumberFormatter implements Formatter<Number> {
+
+    @Override
+    public Number parse(String text, Locale locale) throws ParseException {
+        log.info("text={}, locale={}", text, locale);
+        // "1,000" -> 1000
+        NumberFormat format = NumberFormat.getInstance(locale);
+        return format.parse(text);
+    }
+
+    @Override
+    public String print(Number object, Locale locale) {
+        log.info("object={}, locale={}", object, locale);
+        NumberFormat instance = NumberFormat.getInstance(locale);
+        return NumberFormat.getInstance(locale).format(object);
+    }
+}
