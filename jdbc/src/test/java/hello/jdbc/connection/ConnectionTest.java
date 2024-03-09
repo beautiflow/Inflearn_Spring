a0b175860bc59112b3ca172781ee0c970dab9517
beautiflow <1114ny@naver.com>
2024-03-09 16:09:08 +0900

    feat: Section.2-4 DataSource 예제2 - 커넥션 풀

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -2,6 +2,7 @@ package hello.jdbc.connection;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -28,6 +29,21 @@ public class ConnectionTest {
                 PASSWORD);
         useDataSource(dataSource);
     }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
 
     private void useDataSource(DataSource dataSource) throws SQLException{
         Connection con1 = dataSource.getConnection();
