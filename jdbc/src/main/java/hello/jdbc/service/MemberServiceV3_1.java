3dafc1d670ab472800b1032443c7d49ba56abf51
beautiflow <1114ny@naver.com>
2024-03-10 16:47:47 +0900

    feat: Section.4-8 트랜잭션 문제 해결 - 트랜잭션 AOP 적용

--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -47,12 +47,4 @@ public class MemberServiceV3_1 {
         if (toMember.getMemberId().equals("ex")) {
             throw new IllegalStateException("이체중 예외 발생"); }
     }
-    private void release(Connection con) {
-        if (con != null) {
-            try {
-                con.setAutoCommit(true); //커넥션 풀 고려 con.close();
-            } catch (Exception e) {
-                log.info("error", e);
-            } }
-    }
 }
--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -48,13 +48,4 @@ public class MemberServiceV3_2 {
         if (toMember.getMemberId().equals("ex")) {
             throw new IllegalStateException("이체중 예외 발생"); }
     }
-    private void release(Connection con) {
-        if (con != null) {
-            try {
-                con.setAutoCommit(true); //커넥션 풀 고려 con.close();
-            } catch (Exception e) {
-                log.info("error", e);
-            }
-        }
-    }
 }
