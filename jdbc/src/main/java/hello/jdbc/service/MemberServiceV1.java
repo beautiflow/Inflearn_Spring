ec7fb39dbea1db6b6947073b2c680e86fdb38f52
beautiflow <1114ny@naver.com>
2024-03-10 12:09:19 +0900

    feat: Section.3-11 트랜잭션 - 적용2

--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -11,12 +11,14 @@ public class MemberServiceV1 {
     private final MemberRepositoryV1 memberRepository;
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 시작
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
 
         memberRepository.update(fromId, fromMember.getMoney() - money);
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
+        // 커밋, 롤백
 
     }
 
