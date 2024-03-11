358c81ab68a0e7870b7234c56c202a5c76bfe12f
beautiflow <1114ny@naver.com>
2024-03-11 20:55:20 +0900

    feat: Section.6-5 JDBC 반복 문제 해결 - JdbcTemplate

--- jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -7,6 +7,7 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepositoryV3;
 import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV5;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
@@ -51,7 +52,9 @@ class MemberServiceV4Test {
         @Bean
         MemberRepository memberRepository(){
 //            return new MemberRepositoryV4_1(dataSource);
-            return new MemberRepositoryV4_1(dataSource);
+//            return new MemberRepositoryV4_1(dataSource);
+            return new MemberRepositoryV5(dataSource);
+
 
         }
 
